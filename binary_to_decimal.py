d9685ef1c613b2a4b41b37ce66d1f36a4cc04c0a
Reilly <reilly.ford@gmail.com>
2021-04-24 08:33:07 -0400

    exercise 1.2

--- binary_to_decimal.py
+++ binary_to_decimal.py
@@ -20,7 +20,22 @@ def binary_to_hex(binary_list):
     :param binary_list: A list of ones and zeroes of size 4n.
     :return: A string representing a hex number.
     """
-    pass
+    hex_converter = {"0000": 0, "0001": 1, "0010": 2, "0011": 3, "0100": 4, "0101": 5, "0110": 6, "0111": 7, "1000": 8, "1001": 9, "1010": "a", "1011": "b", "1100": "c", "1101": "d", "1110": "e", "1111": "f"}
+    while len(binary_list) % 4 != 0:
+        binary_list.insert(0, 0)
+    digits_checked = 0
+    segment = ""
+    translation = ""
+    for digit in binary_list:
+        segment += str(digit)
+        digits_checked += 1
+        if digits_checked == 4:
+            segment = hex_converter[segment]
+            translation += str(segment)
+            print(segment)
+            digits_checked = 0
+            segment = ""
+    return translation
 
 
 def main():
@@ -28,13 +43,12 @@ def main():
     Just some sample behavior. Feel free to tru your own!
     """
 
-    binary_list = [1, 0, 0, 1, 1, 1, 0, 1]  # be mindful about not changing this list in your functions
-
+    binary_list = [1, 1, 0, 0, 1, 1, 1, 0, 1]  # be mindful about not changing this list in your functions
     # Problem 1.1
     print(binary_to_decimal(binary_list))
 
     # Problem 1.2
-    # print(binary_to_hex(binary_list))  # uncomment to test
+    print(binary_to_hex(binary_list))  # uncomment to test
 
 # DO NOT WRITE CODE BELOW THIS LINE
 
